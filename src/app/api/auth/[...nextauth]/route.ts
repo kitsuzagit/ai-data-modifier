434625ba47c46f354cfe12c898d08064f06926ac
kitsuza <t.hiroki926@gmail.com>
2026-06-03 15:27:13 +0900

    fix: nextauth route handlers and vars

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,11 @@
-// src/app/api/auth/[...nextauth]/route.ts
-import { handlers } from "../../../../auth";
-export const { GET, POST } = handlers;
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
