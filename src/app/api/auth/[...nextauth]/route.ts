64112dc960c644cd78c40e2767077e98d62a5957
kitsuza <t.hiroki926@gmail.com>
2026-06-04 03:18:51 +0900

    fix auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,3 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import { handlers } from "../../../../auth";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-});
+export const { GET, POST } = handlers;
--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,8 @@
-// src/auth.ts
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Google],
-  callbacks: {
-    // ログインしたユーザーのIDをセッションに含める
-    async session({ session, token }) {
-      if (session.user && token.sub) {
-        session.user.id = token.sub;
-      }
-      return session;
-    },
-  },
+  providers: [
+    Google, // 引数は何も書かなくてOKです！自動で環境変数を認識します
+  ],
 });
